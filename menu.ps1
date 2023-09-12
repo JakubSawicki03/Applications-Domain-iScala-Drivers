11c084cc3dce39f30f4086f9b8228e4793743862
Jakub Sawicki <142387174+JakubSawicki03@users.noreply.github.com>
2023-09-12 21:27:45 +0200

    Update menu.ps1

--- menu.ps1
+++ menu.ps1
@@ -9,7 +9,7 @@ function Show-Menu {
     Write-Host "2: Add computer to a domain"
     Write-Host "3: Install 7-Zip, Firefox oraz Adobe Reader "
     Write-Host "4: Install iScala Client"
-    Write-Host "4: Set default policy"`n
+    Write-Host "5: Set default policy"`n
 
     Write-Host "q: Exit powershell'a"`n
 } 
@@ -23,7 +23,7 @@ do {
         "2" {PATH\domain.ps1}
         "3" {PATH\applications.ps1}
         "4" {PATH\iscala.ps1}
-        "4" {PATH\policy.ps1}
+        "5" {PATH\policy.ps1}
         "q" {exit}
     }
 } 
